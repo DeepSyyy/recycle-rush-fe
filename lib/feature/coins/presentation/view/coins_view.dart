6f60497581a93639f2543925d5c91e14209b8647
Falah <falah@Falahs-MacBook-Air.local>
2024-01-18 21:46:07 +0700

    feature coins

---
+++ lib/feature/coins/presentation/view/coins_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:recycle_rush_fe/feature/coins/presentation/widget/coins_widget.dart';
+
+class RecycleRushViewCoins extends StatelessWidget {
+  static const routeName = '/coins';
+  const RecycleRushViewCoins({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const RecycleRushWidgetCoins();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:recycle_rush_fe/feature/auth/presentation/view/auth_view.dart';
 import 'package:recycle_rush_fe/feature/auth/presentation/widget/sign_in_widget.dart';
 import 'package:recycle_rush_fe/feature/auth/presentation/widget/sign_up_widget.dart';
+import 'package:recycle_rush_fe/feature/coins/presentation/view/coins_view.dart';
+import 'package:recycle_rush_fe/feature/coins/presentation/widget/coins_widget.dart';
+import 'package:recycle_rush_fe/feature/coins/presentation/widget/transfer_widget.dart';
 import 'package:recycle_rush_fe/feature/home/presentation/view/home_view.dart';
 import 'package:recycle_rush_fe/feature/navigation_bar/presentation/view/nav_bar_view.dart';
 import 'package:recycle_rush_fe/feature/onboarding/presentation/view/onboarding_view.dart';
@@ -31,9 +34,13 @@ class MyApp extends StatelessWidget {
             const RecycleRushViewNavBar(),
         RecycleRushWidgetTrashGo.routeName: (context) =>
             const RecycleRushWidgetTrashGo(),
+        RecycleRushViewCoins.routeName: (context) =>
+            const RecycleRushViewCoins(),
+        RecyleRushWidgetTransfer.routeName: (context) =>
+            const RecyleRushWidgetTransfer(),
       },
       // initialRoute: RecycleRushViewHome.routeName,
-      home: const RecycleRushWidgetSplashScreen(),
+      home: const RecycleRushWidgetCoins(),
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
